f93ac008bf6eee8db8257bf497dc9c3b38401124
Mhammed Magdi <sirdonvua@pop-os.localdomain>
2022-09-25 22:21:20 +0300

    added .bashrc & .vimrc

--- func/extrapkg.sh
+++ func/extrapkg.sh
@@ -4,20 +4,32 @@ extra() {
 
     #extra cool packages
 	sudo deb-get install lsd bat
-	sudo nala install cmatrix cowsay lolcat figlet doas -y
+	sudo nala install cmatrix cowsay lolcat figlet -y
 
-	#replacing normal boring ls and cat		
+    #replacing normal boring ls and cat
 	echo "alias ls='lsd'" >> ~/.bashrc && echo "alias lt='lsd --tree'" >> ~/.bashrc
 	echo "alias cat='bat'" >> ~/.bashrc
 
-	#setup the fish shell
+	#starship prompt
+    curl -sS https://starship.rs/install.sh | sh
+
+    #vim-plug
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+    ### CONFIG FILES ###
+    mv $HOME/.bashrc $HOME/.bashrc.old
+    mv $HOME/.vimrc $HOME/.vimrc.old
+    wget https://raw.githubusercontent.com/sirDonVua/mydotfiles/main/.bashrc -O $HOME/.bashrc
+    wget https://raw.githubusercontent.com/sirDonVua/mydotfiles/main/.vimrc -O $HOME/.vimrc
+}
+
+fish() { 
+    #setup the fish shell
 	wget https://launchpad.net/~fish-shell/+archive/ubuntu/release-3/+files/fish_3.5.1-1~jammy_amd64.deb
 	sudo nala install ./fish_3.5.1-1~jammy_amd64.deb -y
 	rm fish_3.5.1-1~jammy_amd64.deb
     #setup the fish config file
     mkdir -p ~/.config/fish/
-    wget "https://raw.githubusercontent.com/sirDonVua/mydotfiles/main/config.fish" -O ~/.config/fish/config.fish         
-    #removing fish gretting
-    touch ./nowelcome.sh ; echo "#!/bin/fish" >> nowelcome.sh ; echo "set -U fish_greeting" >> nowelcome.sh
-    fish nowelcome.sh ; rm -rf ./nowelcome.sh ./run.sh
+    wget https://raw.githubusercontent.com/sirDonVua/mydotfiles/main/config/fishShell/config.fish -O ~/.config/fish/config.fish
 }
