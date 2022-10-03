8c136aabff9eaa487c55f3945edc807c95ba78a4
sirdonvua <sirdonvua@fakeemail.com>
2022-10-03 22:30:15 +0300

    added virt manager

--- func/extra.sh
+++ func/extra.sh
@@ -24,14 +24,7 @@ extra() {
     wget https://raw.githubusercontent.com/vifm/vifm-colors/master/gruvbox.vifm -O $HOME/.config/vifm/colors/gruvbox.vifm
     sed -i 's/colorscheme/colorscheme dracula gruvbox/' $HOME/.config/vifm/vifmrc
 
-}
+    #virt manager
+    sudo nala install virt-manager && sudo systemctl enable libvirtd
 
-fish() { 
-    #setup the fish shell
-	wget https://launchpad.net/~fish-shell/+archive/ubuntu/release-3/+files/fish_3.5.1-1~jammy_amd64.deb
-	sudo nala install ./fish_3.5.1-1~jammy_amd64.deb -y
-	rm fish_3.5.1-1~jammy_amd64.deb
-    #setup the fish config file
-    mkdir -p ~/.config/fish/
-    wget https://raw.githubusercontent.com/sirDonVua/mydotfiles/main/config/fishShell/config.fish -O ~/.config/fish/config.fish
 }
